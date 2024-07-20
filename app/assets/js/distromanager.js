d1dbcd315cd3c8325b2256698cebad4e1e8a6e6f
kipawepro <kipawe.pro@gmail.com>
2024-07-20 17:43:51 +0200

    Add all file

---
+++ app/assets/js/distromanager.js
@@ -0,0 +1,15 @@
+const { DistributionAPI } = require('helios-core/common')
+
+const ConfigManager = require('./configmanager')
+
+exports.REMOTE_DISTRO_URL = 'https://kittyteam.wstr.fr/kittytown/distribution.json'
+
+const api = new DistributionAPI(
+    ConfigManager.getLauncherDirectory(),
+    null, // Injected forcefully by the preloader.
+    null, // Injected forcefully by the preloader.
+    exports.REMOTE_DISTRO_URL,
+    false
+)
+
+exports.DistroAPI = api
